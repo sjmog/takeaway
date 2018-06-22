2d2ccd79f3f1d992c687b9f05f2864e1c05040ab
Sam Morgan <s_morgan@me.com>
2018-06-22 13:05:01 +0100

    The user can just run `takeaway` from the command-line
    * This is a nicer user interaction.
    * [Here](https://commandercoriander.net/blog/2013/02/16/making-a-ruby-script-executable/) is a helpful article on how to do this.
    * The steps are:
      * Change the feature test to use a command instead: `takeaway`.
      * Create a new Ruby script, with the shebang and the Ruby env
    reference #!/usr/bin/env ruby. Name it `takeaway`. No extension needed!
    Fill with smoke test content for now.
      * Make the script executable by changing the permissions: chmod 755
    takeaway.
      * To allow us to run the script without referencing this location
    specifically, softlink it to a directory in the $PATH, like
    /usr/local/bin. The article explains more.
      * Move the code from app.rb into the script.

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Interacting with the program as a user" do
 
   it 'See a list of dishes with prices' do
     # Run the list program and capture the result
-    message_printed_to_stdout = `ruby app.rb`
+    message_printed_to_stdout = `takeaway`
     # Expect that result to get a list of dishes with prices
     expect(message_printed_to_stdout).to eq "Pizza: $10\nEel Pizza: $3\n"
   end
