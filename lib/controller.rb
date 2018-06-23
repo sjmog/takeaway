d16eadd5df51344c11886deedeae38c00074d1d6
Sam Morgan <s_morgan@me.com>
2018-06-24 00:08:56 +0100

    Minor refactor of the controller
    * Extract the start logic to an instance method, which can now be
    extracted further using private methods.

--- lib/controller.rb
+++ lib/controller.rb
@@ -2,16 +2,25 @@ require_relative './takeaway'
 
 # Understands routing the commands given to the program
 class Controller
+  def initialize(input, takeaway)
+    @input    = input
+    @takeaway = takeaway
+  end
+
    def self.start(input = $stdin, takeaway = Takeaway.new)
+    new(input, takeaway).start
+  end
+
+  def start
     loop do
-      input_string = input.gets || "exit"
+      input_string = @input.gets || "exit"
       command      = input_string.split(" ").first
 
       if command == "exit"
-        takeaway.exit
+        @takeaway.exit
         break
       elsif command == "list"
-        takeaway.list
+        @takeaway.list
       end
     end
   end
